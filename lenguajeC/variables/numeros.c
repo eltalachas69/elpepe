a64709de29a3cb263d3e6ddf9414b24a314f11cb
ElCuilango <juancrls760@gmail.com>
2026-06-13 17:27:30 -0600

    ahora tiene decimal y letra y culero

--- lenguajeC/variables/numeros.c
+++ lenguajeC/variables/numeros.c
@@ -3,8 +3,12 @@
 int main()
 {
     int numero = 131;
+    float decimal = 12.12;
+    char letra = 'Q';
 
-    printf("%d", numero);
+    printf("%d\n", numero);
+    printf("%f\n", decimal);
+    printf("%c\n", letra);
     printf("\nculero");
 
     return 0;
