2c2b43cdcbe7fd7c27774b6f213ed6466bb46361
ElCuilango <juancrls760@gmail.com>
2026-06-13 11:12:37 -0600

    mamadas

---
+++ lenguajeC/holamundo/hola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Este es mi programa donde hace un hola mundo
+ *
+ */
+int main()
+{
+    printf("Hola mundo");
+    return 0;
+}
---
+++ lenguajeC/holamundo/holanombre.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Este es mi programa donde dice 'Hola culero'
+ */
+int main()
+{
+    char name[] = "Culero";
+    printf("Hola %s", name);
+    printf(" \n Que tengas un bonito dia :)");
+    return 0;
+}
