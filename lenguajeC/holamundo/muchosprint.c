4fea4e8ac3ecaac7ada081c6d3cd5cc93012d3d2
ElCuilango <juancrls760@gmail.com>
2026-06-13 11:29:20 -0600

    Varios printf

---
+++ lenguajeC/holamundo/muchosprint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char texto[] = "Culeros y puñales";
+    printf("Hola mundo ");
+    printf("Estoy viendo C ");
+    printf("Putos ");
+    printf(texto);
+    return 0;
+}
