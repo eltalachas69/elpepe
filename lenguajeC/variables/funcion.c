35a02727cc966fa05ec931e5c22b9b5dcae9e092
ElCuilango <juancrls760@gmail.com>
2026-06-13 17:50:04 -0600

    mas variapopo y una funcion

---
+++ lenguajeC/variables/funcion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int numeroA = 12;
+int numeroB = 10;
+
+int suma()
+{
+    return numeroA + numeroB;
+}
+
+int main()
+{
+    printf("%d", suma());
+    return 0;
+}
---
+++ lenguajeC/variables/otronumr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int numero = 67;
+    char letra = 'B';
+    printf("Mi numero favorito es %d y mi letra favorita es la %c", numero, letra);
+    return 0;
+}
