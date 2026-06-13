d027e5edfd8109b4bc910a94f2798b82dc922433
ElCuilango <juancrls760@gmail.com>
2026-06-13 17:22:18 -0600

    mas mamadas

---
+++ lenguajeC/holamundo/saltoslineas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Esto es una linea\n");
+    printf("Saltado en otra linea\n");
+    printf("Esto lo hice en una linea \ncon solo una linea");
+    return 0;
+}
---
+++ lenguajeC/variables/numeros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 131;
+
+    printf("%d", numero);
+    printf("\nculero");
+
+    return 0;
+}
